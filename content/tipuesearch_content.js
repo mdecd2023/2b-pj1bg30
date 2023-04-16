dd15c6effbe50b6fa0b58c6190da3faf950e369a
41023230 <41023230@gm.nfu.edu.tw>
2023-04-17 00:21:33 +0800

    add bubblerob

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var tipuesearch = {"pages": [{'title': 'About', 'text': '41023230 彭聖宗 \n 41023232 雲敬家 \n 倉儲 https://github.com/mdecd2023/2b-pj1bg30 \n', 'tags': '', 'url': 'About.html'}, {'title': 'Tutorial', 'text': '', 'tags': '', 'url': 'Tutorial.html'}]};
-=======
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2b-pj1bg30 \n 網站: https://mdecd2023.github.io/2b-pj1bg30/content/index.html', 'tags': '', 'url': 'About.html'}]};
->>>>>>> 37115a5c187a4829db8bbd7ad451944ec1477bcc
+var tipuesearch = {"pages": [{'title': 'About', 'text': '<<<<<<< HEAD \n 41023230 彭聖宗 \n 41023232 雲敬家 \n 倉儲 https://github.com/mdecd2023/2b-pj1bg30 \n', 'tags': '', 'url': 'About.html'}, {'title': 'HW', 'text': '', 'tags': '', 'url': 'HW.html'}, {'title': 'Tutorial', 'text': '\n', 'tags': '', 'url': 'Tutorial.html'}, {'title': 'BubbleRob', 'text': '======= \n 倉儲: https://github.com/mdecd2023/2b-pj1bg30 \n 網站: https://mdecd2023.github.io/2b-pj1bg30/content/index.html \n >>>>>>> 37115a5c187a4829db8bbd7ad451944ec1477bcc', 'tags': '', 'url': 'BubbleRob.html'}]};
